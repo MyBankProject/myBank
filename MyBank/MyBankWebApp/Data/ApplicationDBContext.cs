78d59ef4217500ba33d49fbc171d972a834651fa
Michal Lukaszewicz <michal.lukaszewicz@precast.pl>
2025-02-17 20:21:10 +0100

    Added ApplicationDbContext class

---
+++ MyBank/MyBankWebApp/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBankWebApp.Models;
+
+namespace MyBankWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<AccountDetail> AccountDetails { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
