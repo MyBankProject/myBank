e35e62efcdd6d5b8c7e19ddbbbf263120c92b6aa
Michal Lukaszewicz <michal.lukaszewicz@precast.pl>
2025-02-17 19:25:17 +0100

    Created boilerplate of BankService controller and View

---
+++ MyBank/MyBankWebApp/Controllers/BankServiceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBankWebApp.Controllers
+{
+    public class BankServiceController : Controller
+    {
+        public BankServiceController()
+        {
+            //TODO: Dodać w konstruktorze model jednego użytkownika - na razie trzeba go najpierw stworzyć
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
