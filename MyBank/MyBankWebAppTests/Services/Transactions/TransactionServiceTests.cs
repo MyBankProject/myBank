904a6f83ea69e5704ef81547dddff2f4446131fb
Michal Lukaszewicz <michal.lukaszewicz@precast.pl>
2025-03-16 09:07:07 +0100

    Created TransactionServiceTests

--- MyBank/MyBankWebAppTests/Services/Transactions/TransactionServiceTests.cs
+++ MyBank/MyBankWebAppTests/Services/Transactions/TransactionServiceTests.cs
@@ -6,10 +6,10 @@ using MyBankWebApp.Mappers;
 using MyBankWebApp.Models;
 using MyBankWebApp.Models.Abstractions;
 using MyBankWebApp.Repositories.Abstractions;
+using MyBankWebApp.Services.Transactions;
 using MyBankWebApp.ViewModels;
-using System.Data.Common;
 
-namespace MyBankWebApp.Services.Transactions
+namespace MyBankWebAppTests.Services.Transactions
 {
     [TestClass]
     public class TransactionServiceTests
@@ -22,6 +22,76 @@ namespace MyBankWebApp.Services.Transactions
         private Mock<IAccountDetail> sender;
         private TransactionService sut;
         private Mock<ITransactionRepository> transactionRepository;
+
+        [TestMethod]
+        public async Task AddTransactionAsync_CreatesTransactionCorrectly()
+        {
+            //Arrange
+            sender.SetupProperty(_ => _.Balance, 100);
+            reciver.SetupProperty(_ => _.Balance, 100);
+            var transaction = new Mock<IDbContextTransaction>();
+            transactionRepository.Setup(_ => _.BeginTransactionAsync()).ReturnsAsync(transaction.Object);
+            Transaction resultTransaction = null;
+            transactionRepository.Setup(_ => _.AddAsync(It.IsAny<Transaction>())).Callback<Transaction>(tsct => resultTransaction = tsct);
+
+            const int senderId = 1;
+            var transactionVM = new NewTransactionViewModel()
+            {
+                ReciverIBAN = default_Iban,
+                Amount = 100.00m,
+                Description = "description",
+                ReciverName = "Reciver Name",
+                TransferDate = DateTime.Now,
+                SenderId = senderId
+            };
+
+            //Act
+            await sut.AddTransactionAsync(transactionVM);
+
+            //Assert
+            accountDetailsRepository.Verify(_ => _.GetAccountByIbanAsync(It.IsAny<string>()), Times.Once());
+            accountDetailsRepository.Verify(_ => _.GetAccountByIbanAsync(IbanNumbersOnly), Times.Once());
+            accountDetailsRepository.Verify(_ => _.GetByIdAsync(It.IsAny<int>(), null), Times.Once);
+            accountDetailsRepository.Verify(_ => _.GetByIdAsync(senderId, null), Times.Once);
+            transactionRepository.Verify(_ => _.BeginTransactionAsync(), Times.Once);
+            sender.VerifySet(_ => _.Balance = 0.00m, Times.Once());
+            reciver.VerifySet(_ => _.Balance = 200.00m, Times.Once());
+            transaction.Verify(_ => _.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
+            transactionRepository.Verify(_ => _.AddAsync(It.IsAny<Transaction>()), Times.Once);
+            transactionRepository.Verify(_ => _.SaveAsync(), Times.Once);
+            Assert.IsNotNull(resultTransaction);
+            Assert.AreSame(sender.Object, resultTransaction.SenderAccountDetails);
+        }
+
+        [TestMethod]
+        public async Task AddTransactionAsync_ErrorDuringTransaction_RollsBackAndThrowsException()
+        {
+            //Arrange
+            sender.SetupProperty(_ => _.Balance, 100);
+            reciver.SetupProperty(_ => _.Balance, 100);
+            var transaction = new Mock<IDbContextTransaction>();
+            transactionRepository.Setup(_ => _.BeginTransactionAsync()).ReturnsAsync(transaction.Object);
+            transactionRepository.Setup(_ => _.AddAsync(It.IsAny<Transaction>())).ThrowsAsync(new Exception());
+
+            const int senderId = 1;
+            var transactionVM = new NewTransactionViewModel()
+            {
+                ReciverIBAN = default_Iban,
+                Amount = 100.00m,
+                SenderId = senderId
+            };
+
+            //Act
+            await Assert.ThrowsExceptionAsync<Exception>(() => sut.AddTransactionAsync(transactionVM));
+
+            //Assert
+            accountDetailsRepository.Verify(_ => _.GetAccountByIbanAsync(It.IsAny<string>()), Times.Once());
+            accountDetailsRepository.Verify(_ => _.GetAccountByIbanAsync(IbanNumbersOnly), Times.Once());
+            accountDetailsRepository.Verify(_ => _.GetByIdAsync(It.IsAny<int>(), null), Times.Once);
+            accountDetailsRepository.Verify(_ => _.GetByIdAsync(senderId, null), Times.Once);
+            transactionRepository.Verify(_ => _.BeginTransactionAsync(), Times.Once);
+            transaction.Verify(_ => _.RollbackAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
 
         [TestMethod]
         public async Task AddTransactionAsync_NotEnoughBalance_ThrowsException()
@@ -45,70 +115,6 @@ namespace MyBankWebApp.Services.Transactions
             accountDetailsRepository.Verify(_ => _.GetAccountByIbanAsync(IbanNumbersOnly), Times.Once());
             accountDetailsRepository.Verify(_ => _.GetByIdAsync(It.IsAny<int>(), null), Times.Once);
             accountDetailsRepository.Verify(_ => _.GetByIdAsync(senderId, null), Times.Once);
-        }
-
-        [TestMethod]
-        public async Task AddTransactionAsync_ErrorDuringTransaction_RollsBackAndThrowsException()
-        {
-            //Arrange
-            sender.SetupProperty(_ => _.Balance, 100);
-            reciver.SetupProperty(_ => _.Balance, 100);
-            var transaction = new Mock<IDbContextTransaction>();
-            transactionRepository.Setup(_ => _.BeginTransactionAsync()).ReturnsAsync(transaction.Object);
-
-            const int senderId = 1;
-            var transactionVM = new NewTransactionViewModel()
-            {
-                ReciverIBAN = default_Iban,
-                Amount = 100.00m,
-                SenderId = senderId
-            };
-
-            //Act
-            await Assert.ThrowsExceptionAsync<NullReferenceException>(() => sut.AddTransactionAsync(transactionVM));
-
-            //Assert
-            accountDetailsRepository.Verify(_ => _.GetAccountByIbanAsync(It.IsAny<string>()), Times.Once());
-            accountDetailsRepository.Verify(_ => _.GetAccountByIbanAsync(IbanNumbersOnly), Times.Once());
-            accountDetailsRepository.Verify(_ => _.GetByIdAsync(It.IsAny<int>(), null), Times.Once);
-            accountDetailsRepository.Verify(_ => _.GetByIdAsync(senderId, null), Times.Once);
-            transactionRepository.Verify(_ => _.BeginTransactionAsync(), Times.Once);
-            transaction.Verify(_ => _.RollbackAsync(It.IsAny<CancellationToken>()), Times.Once);
-        }
-
-        [TestMethod]
-        public async Task AddTransactionAsync_CreatesTransactionCorrectly()
-        {
-            //Arrange
-            sender.SetupProperty(_ => _.Balance, 100);
-            reciver.SetupProperty(_ => _.Balance, 100);
-            var transaction = new Mock<IDbContextTransaction>();
-            transactionRepository.Setup(_ => _.BeginTransactionAsync()).ReturnsAsync(transaction.Object);
-
-            const int senderId = 1;
-            var transactionVM = new NewTransactionViewModel()
-            {
-                ReciverIBAN = default_Iban,
-                Amount = 100.00m,
-                Description = "description",
-                ReciverName = "Reciver Name",
-                TransferDate = DateTime.Now,
-                SenderId = senderId
-            };
-
-            //Act
-            await sut.AddTransactionAsync(transactionVM));
-
-            //Assert
-            accountDetailsRepository.Verify(_ => _.GetAccountByIbanAsync(It.IsAny<string>()), Times.Once());
-            accountDetailsRepository.Verify(_ => _.GetAccountByIbanAsync(IbanNumbersOnly), Times.Once());
-            accountDetailsRepository.Verify(_ => _.GetByIdAsync(It.IsAny<int>(), null), Times.Once);
-            accountDetailsRepository.Verify(_ => _.GetByIdAsync(senderId, null), Times.Once);
-            transactionRepository.Verify(_ => _.BeginTransactionAsync(), Times.Once);
-            sender.VerifySet(_ => _.Balance = 0.00m, Times.Once());
-            reciver.VerifySet(_ => _.Balance = 200.00m, Times.Once());
-            transaction.Verify(_ => _.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
-            Assert.Fail("Dokończyć pisać test");
         }
 
         [TestInitialize]
