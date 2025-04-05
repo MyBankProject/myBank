15fabe0a36327f8303cf410fbfbaacf162d11be4
Michal Lukaszewicz <michal.lukaszewicz@precast.pl>
2025-04-05 17:40:01 +0200

    Updated Roating after login/registering

--- MyBank/MyBankWebApp/Controllers/UserController.cs
+++ MyBank/MyBankWebApp/Controllers/UserController.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MyBankWebApp.DTOs;
 using MyBankWebApp.DTOs.Creates;
 using MyBankWebApp.Models;
-using MyBankWebApp.Services;
 using MyBankWebApp.Services.UserServices.Abstractions;
 
 namespace MyBankWebApp.Controllers
@@ -17,15 +15,36 @@ namespace MyBankWebApp.Controllers
             this.userService = userService;
         }
 
-        public IActionResult Register()
-        {
-            return View();
-        }
-
         [HttpGet]
         public IActionResult Login(string? error = null)
         {
             ViewBag.LoginError = error;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginDto dto)
+        {
+            string token = userService.GenerateJwt(dto);
+
+            Response.Cookies.Append("AuthToken", token, new CookieOptions
+            {
+                HttpOnly = true, // Zabezpiecza przed dostępem przez JavaScript
+                Secure = true, // Włączone dla HTTPS
+                Expires = DateTime.UtcNow.AddHours(1) // Token ważny przez 1 godzinę
+            });
+            return RedirectToAction("Index", "BankService");
+        }
+
+        // [HttpPost] bedzie potrzebne zeby wylogowac sie poprzez klikniecie guzika.. poki co dziala przez link
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("AuthToken");
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Register()
+        {
             return View();
         }
 
@@ -57,29 +76,6 @@ namespace MyBankWebApp.Controllers
                 }
                 return View("Register", model);
             }
-            return RedirectToAction("SuccessRegister");
-        }
-
-
-        [HttpPost]
-        public IActionResult Login(LoginDto dto)
-        {
-            string token = userService.GenerateJwt(dto);
-
-            Response.Cookies.Append("AuthToken", token, new CookieOptions
-            {
-                HttpOnly = true, // Zabezpiecza przed dostępem przez JavaScript
-                Secure = true, // Włączone dla HTTPS
-                Expires = DateTime.UtcNow.AddHours(1) // Token ważny przez 1 godzinę
-            });
-            //return RedirectToAction("SuccessLogin"); // trzeba bedzie gdzies przekierowac zamiast wyswietlac tokena xD
-            return Content(token);
-        }
-
-       // [HttpPost] bedzie potrzebne zeby wylogowac sie poprzez klikniecie guzika.. poki co dziala przez link
-        public IActionResult Logout()
-        {
-            Response.Cookies.Delete("AuthToken");
             return RedirectToAction("Index", "Home");
         }
 
